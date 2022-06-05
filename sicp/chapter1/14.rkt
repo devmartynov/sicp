9fd4b44418579204cc85242a3d01717b7a6ad1bf
dmartynov <denis.martynov.93@mail.ru>
2022-06-05 08:51:01 +0300

    ex 14

---
+++ sicp/chapter1/14.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (a-plus-abs-b a b) 
+	((if (> b 0) + -) a b))
+
+; How it works?
+; 1. evaluate predicat.
+; 2. if predicat is true return "+" procedure else return "-" procedure.
+; 3. evaluate a and b
+; 4. evaluate procedure from step2 with arguments from step 3. 
+
+check-equal? (a-plus-abs-b 1 (- 1)) 2
