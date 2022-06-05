2b05bb05f37bf296d8df09473e266f90382d18df
dmartynov <denis.martynov.93@mail.ru>
2022-06-05 08:42:20 +0300

    ex 13

---
+++ sicp/chapter1/13.rkt
@@ -0,0 +1,26 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (square a) (* a a))
+
+(define (sum-of-squares a b) (+ (square a) (square b)))
+
+
+(define (sum-of-square-two-biggest-numbers a b c) 
+	(cond 
+		((> a b) (if (> b c) 
+			(sum-of-squares a b)
+			(sum-of-squares a c)))
+		((> a c) (sum-of-squares a b))
+		(else (sum-of-squares b c))))
+
+check-equal? (square 5) 25
+check-equal? (square 4) 16
+
+check-equal? (sum-of-squares 2 2) 8
+check-equal? (sum-of-squares 3 5) 34
+
+check-equal? (sum-of-square-two-biggest-numbers 1 2 3) 13
+check-equal? (sum-of-square-two-biggest-numbers 5 1 3) 34
+check-equal? (sum-of-square-two-biggest-numbers 4 4 6) 52 
